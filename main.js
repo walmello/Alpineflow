d008ab23aef8ba42f2ab40e7c66695e71700788c
Walter Melo <41174751+walmello24@users.noreply.github.com>
2022-11-07 16:57:46 -0300

    Add gitignore

--- main.js
+++ main.js
@@ -1,5 +1,62 @@
 document.addEventListener('alpine:init', () => {
-    Alpine.directive('alert', (el, {exp}) => {
-        alert(exp)
+    Alpine.directive('alert', (el, {expression}) => {
+        alert(expression)
+    })
+
+    const setTag = (el, tag) => {
+        let copy = document.createElement(tag)
+        copy.innerHTML = el.innerHTML;
+        [...el.attributes].forEach(attr => {
+            copy.setAttribute(attr.nodeName, attr.nodeValue)
+        })
+        return copy
+    }
+
+    Alpine.directive('tag', (el, {expression}) => {
+        let copy = setTag(el, expression)
+        copy.removeAttribute('x-tag')
+        el.parentNode.replaceChild(copy, el)
+    })
+
+    Alpine.directive('template', (el, {expression}) => {
+        let copy = document.createElement(expression)
+        copy.innerHTML = el.innerHTML;
+        [...el.attributes].forEach(attr => {
+            copy.setAttribute(attr.nodeName, attr.nodeValue)
+        })
+        copy.removeAttribute('x-template')
+        el.parentNode.replaceChild(copy, el)
+    })
+
+    Alpine.directive('mod', (el, {expression}) => {
+        let attrs = [...el.attributes]
+            .filter(item => item.nodeName.split('-')[0] == 'x')
+            .map(attr => attr.name)
+            /*
+            .map(attr => {
+                attr = attr.split(':').join('.')
+                attr = attr.replace(/\./, ':')
+
+                return attr
+            })
+            */
+
+        let newAttrs = attrs.map(attr => {
+            attr = attr.split(':').join('.')
+            attr = attr.replace(/\./, ':')
+
+            return attr
+        })
+
+        newAttrs.forEach((attr, index) => {
+            el.setAttribute(attr,el.getAttribute(attrs[index]))
+        })
+
+        attrs.forEach(attr => {
+            el.removeAttribute(attr)
+        })
+
+        el.removeAttribute('x-mod')            
+        console.log(attrs)
     })
 })
