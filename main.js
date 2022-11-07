ed035fb22ed0a45c0d7d695e5eca4f0464d49925
Walter Melo <41174751+walmello24@users.noreply.github.com>
2022-11-07 19:35:57 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,43 +3,51 @@ document.addEventListener('alpine:init', () => {
         alert(expression)
     })
 
-    const setTag = (el, tag) => {
-        let copy = document.createElement(tag)
-        copy.innerHTML = el.innerHTML;
-        [...el.attributes].forEach(attr => {
-            copy.setAttribute(attr.nodeName, attr.nodeValue)
+    const setTemplate = (name, action = () => {}) => {
+        console.log('calledDirective')
+        Alpine.directive(name, (el, {expression}) => {
+            let copy = document.createElement('template')
+            copy.innerHTML = el.innerHTML;
+            [...el.attributes].forEach(attr => {
+                copy.setAttribute(attr.nodeName, attr.nodeValue)
+            })
+            copy.removeAttribute('x-'+name)
+            action(el, expression)
+            el.parentNode.replaceChild(copy, el)
         })
-        return copy
     }
 
+    setTemplate('template')
+    setTemplate('foreach', (el, expression) => {
+        el.setAttribute('x-for', expression)
+    })
+
+    /*
     Alpine.directive('tag', (el, {expression}) => {
         let copy = setTag(el, expression)
         copy.removeAttribute('x-tag')
         el.parentNode.replaceChild(copy, el)
     })
+    */
 
-    Alpine.directive('template', (el, {expression}) => {
-        let copy = document.createElement(expression)
-        copy.innerHTML = el.innerHTML;
-        [...el.attributes].forEach(attr => {
-            copy.setAttribute(attr.nodeName, attr.nodeValue)
-        })
+    /*
+
+    Alpine.directive('template', (el) => {
+        let copy = setTemplate(el)
         copy.removeAttribute('x-template')
         el.parentNode.replaceChild(copy, el)
     })
 
-    Alpine.directive('mod', (el, {expression}) => {
+    Alpine.directive('foreach', (el ,{expression}) => {
+        let copy = setTemplate(el)
+        copy
+    })
+    */
+
+    Alpine.directive('mods', (el, {expression}) => {
         let attrs = [...el.attributes]
             .filter(item => item.nodeName.split('-')[0] == 'x')
             .map(attr => attr.name)
-            /*
-            .map(attr => {
-                attr = attr.split(':').join('.')
-                attr = attr.replace(/\./, ':')
-
-                return attr
-            })
-            */
 
         let newAttrs = attrs.map(attr => {
             attr = attr.split(':').join('.')
@@ -56,7 +64,6 @@ document.addEventListener('alpine:init', () => {
             el.removeAttribute(attr)
         })
 
-        el.removeAttribute('x-mod')            
-        console.log(attrs)
+        el.removeAttribute('x-mods')            
     })
 })
